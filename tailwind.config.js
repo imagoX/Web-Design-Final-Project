5e875a8777847b6ccdcdaea419cce214875c2043
imago <mohammad.nakhaei.r3@gmail.com>
2023-10-29 10:14:14 +0330

    Header Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        extend: {
+            fontFamily: {
+                'iYekan': ['iranYekan']
+            }
+        },
+    },
+    plugins: [],
+    darkMode: 'class',
 }
 
